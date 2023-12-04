e8863804b6dca37eab6e8e88dfd6fbe4081eee20
dhgh8875 <dhgh8875@naver.com>
2023-12-04 20:56:36 +0900

    Step1

---
+++ SwiftUIMemo/MainListView.swift
@@ -0,0 +1,18 @@
+//
+//  MainListView.swift
+//  SwiftUIMemo
+//
+//  Created by 서경환 on 12/4/23.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainListView()
+}
